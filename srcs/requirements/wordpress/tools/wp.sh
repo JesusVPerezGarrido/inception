b147dd5ec9a59f805b1f220ad1b87c9273715638
JesusVPerezGarrido <jv.perezgarrido@gmail.com>
2025-08-06 12:11:27 +0200

    fixed some missmatches

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -4,10 +4,6 @@
 if [ ! -f wp-config.php ]; then
     echo "Installing WordPress..."
     wp core download --allow-root
-	echo "DB Name: $MYSQL_DATABASE"
-	echo "DB User: $MYSQL_USER"
-	echo "DB Pass: $MYSQL_PASSWORD"
-	echo "DB Host: $MYSQL_HOSTNAME"
     wp config create \
         --dbname=$MYSQL_DATABASE \
         --dbuser=$MYSQL_USER \
@@ -18,7 +14,7 @@ if [ ! -f wp-config.php ]; then
         --url="https://$DOMAIN_NAME" \
         --title="$WORDPRESS_TITLE" \
         --admin_user="$WORDPRESS_ADMIN" \
-        --admin_password="$WORDPRESS_ADMIN_PASS" \
+        --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
         --admin_email="$WORDPRESS_ADMIN_EMAIL" \
         --skip-email \
         --allow-root
@@ -26,8 +22,8 @@ if [ ! -f wp-config.php ]; then
 	wp db check --allow-root
 
     # Create additional regular user
-    wp user create "$WORDPRESS_USER" "$WORDPRESS_EMAIL" \
-        --user_pass="$WORDPRESS_USER_PASS" \
+    wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" \
+        --user_pass="$WORDPRESS_USER_PASSWORD" \
         --allow-root
 
     wp theme install storm-diary --activate --allow-root
